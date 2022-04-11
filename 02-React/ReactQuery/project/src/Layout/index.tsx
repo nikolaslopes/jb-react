c2f4d956e85d38fbd766117c8d4aabed6ce0caf4
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-11 08:05:30 -0400

    :fire:

--- 02-React/ReactQuery/project/src/Layout/index.tsx
+++ 02-React/ReactQuery/project/src/Layout/index.tsx
@@ -1,24 +1,41 @@
-import { Avatar, Box, Grid, Sidebar } from 'grommet';
+import { Avatar, Box, Button, Grid, Main, Nav, Sidebar } from 'grommet';
 
 import { LayoutProps } from './types/LayoutProps';
 
 import avatarImage from '../assets/Icons/avatar.jpg';
+import { Home, Projects } from 'grommet-icons';
+
+import './styles.scss';
 
 export function Layout({ children }: LayoutProps) {
   return (
     <Grid
       rows={['100vh']}
       columns={['auto', 'flex']}
-      areas={[{ name: 'nav', start: [0, 0], end: [0, 0] }]}
+      areas={[
+        { name: 'nav', start: [0, 0], end: [0, 0] },
+        {
+          name: 'main',
+          start: [1, 0],
+          end: [1, 0],
+        },
+      ]}
     >
       <Box gridArea="nav" margin="small">
         <Sidebar
           background="brand"
           round="small"
           header={<Avatar src={avatarImage} />}
-        ></Sidebar>
+        >
+          <Nav gap="small">
+            <Button icon={<Home />} />
+            <Button icon={<Projects />} />
+          </Nav>
+        </Sidebar>
       </Box>
-      {children}
+      <Box gridArea="main" background="light-2" margin="small">
+        <Main pad="large">{children}</Main>
+      </Box>
     </Grid>
   );
 }
