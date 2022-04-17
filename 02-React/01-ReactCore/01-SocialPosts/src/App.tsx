07d2719ff08d23b8f07c424207bafe947d966d9a
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 01:21:53 -0400

    :fire:

--- 02-React/01-ReactCore/01-SocialPosts/src/App.tsx
+++ 02-React/01-ReactCore/01-SocialPosts/src/App.tsx
@@ -1,21 +1,48 @@
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    avatarUrl:
+      'https://i.pinimg.com/originals/65/4d/1d/654d1df29a42e3929f95c353198f60d7.jpg',
+    userName: 'Nix',
+    userRole: 'Full-stack developer',
+    userPostContent: 'I like sass ;D',
+  },
+  {
+    avatarUrl:
+      'https://i.pinimg.com/originals/6e/e7/fa/6ee7fa1ed8dd42b318929c6ca9555764.jpg',
+    userName: 'Sanji',
+    userRole: 'Backend developer',
+    userPostContent: 'I like foods!',
+  },
+  {
+    avatarUrl:
+      'https://i.pinimg.com/originals/32/72/54/327254ba58a75301ae05397957d20f12.jpg',
+    userName: 'Batman',
+    userRole: 'Hero',
+    userPostContent: 'I do not like bats',
+  },
+  {
+    avatarUrl:
+      'https://i.pinimg.com/originals/c8/b0/aa/c8b0aa09026f372342d6fef97800bda3.jpg',
+    userName: 'Luffy',
+    userRole: 'Captain',
+    userPostContent: "I'm gonna the pirates king!",
+  },
+]
+
 function App() {
   return (
     <div className="App">
-      <Post
-        avatarUrl="https://i.pinimg.com/originals/65/4d/1d/654d1df29a42e3929f95c353198f60d7.jpg"
-        userName="Nix"
-        userRole="Full-stack developer"
-        userPostContent="I like sass ;D"
-      />
-
-      <Post
-        avatarUrl="https://i.pinimg.com/originals/6e/e7/fa/6ee7fa1ed8dd42b318929c6ca9555764.jpg"
-        userName="Sanji"
-        userRole="Backend developer"
-        userPostContent="I like foods!"
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.userName}
+          avatarUrl={post.avatarUrl}
+          userName={post.userName}
+          userRole={post.userRole}
+          userPostContent={post.userPostContent}
+        />
+      ))}
     </div>
   )
 }
