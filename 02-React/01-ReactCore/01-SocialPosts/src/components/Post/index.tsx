ffc9e1a8f63f37367e9d31a66ff2c5ed5126c90c
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 02:16:22 -0400

    :fire:

--- 02-React/01-ReactCore/01-SocialPosts/src/components/Post/index.tsx
+++ 02-React/01-ReactCore/01-SocialPosts/src/components/Post/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Like } from '../Like'
 import './styles.css'
 
 type PostProps = {
@@ -20,7 +21,7 @@ export function Post({
     setLiked(true)
   }
 
-  function deslike() {
+  function unLike() {
     setLiked(false)
   }
 
@@ -36,39 +37,7 @@ export function Post({
 
       <p>{userPostContent}</p>
       <section className="actions">
-        {liked ? (
-          <svg
-            onClick={deslike}
-            xmlns="http://www.w3.org/2000/svg"
-            width="44"
-            height="44"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            fill="#ff5e95"
-            stroke="#ff5e95"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-          </svg>
-        ) : (
-          <svg
-            onClick={like}
-            xmlns="http://www.w3.org/2000/svg"
-            width="44"
-            height="44"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#2c3e50"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-          </svg>
-        )}
+        <Like liked={liked} like={like} unLike={unLike} />
       </section>
     </div>
   )
