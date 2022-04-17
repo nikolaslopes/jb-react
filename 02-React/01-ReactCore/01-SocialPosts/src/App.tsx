dc8a03e420050de09c381393ba1b9c86b6fb7df1
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 01:33:41 -0400

    :fire:

--- 02-React/01-ReactCore/01-SocialPosts/src/App.tsx
+++ 02-React/01-ReactCore/01-SocialPosts/src/App.tsx
@@ -2,6 +2,7 @@ import { Post } from './components/Post'
 
 const posts = [
   {
+    id: Math.random(),
     avatarUrl:
       'https://i.pinimg.com/originals/65/4d/1d/654d1df29a42e3929f95c353198f60d7.jpg',
     userName: 'Nix',
@@ -9,6 +10,7 @@ const posts = [
     userPostContent: 'I like sass ;D',
   },
   {
+    id: Math.ceil(Math.random() * 100),
     avatarUrl:
       'https://i.pinimg.com/originals/6e/e7/fa/6ee7fa1ed8dd42b318929c6ca9555764.jpg',
     userName: 'Sanji',
@@ -16,6 +18,7 @@ const posts = [
     userPostContent: 'I like foods!',
   },
   {
+    id: Math.ceil(Math.random() * 100),
     avatarUrl:
       'https://i.pinimg.com/originals/32/72/54/327254ba58a75301ae05397957d20f12.jpg',
     userName: 'Batman',
@@ -23,6 +26,7 @@ const posts = [
     userPostContent: 'I do not like bats',
   },
   {
+    id: Math.ceil(Math.random() * 100),
     avatarUrl:
       'https://i.pinimg.com/originals/c8/b0/aa/c8b0aa09026f372342d6fef97800bda3.jpg',
     userName: 'Luffy',
@@ -36,7 +40,7 @@ function App() {
     <div className="App">
       {posts.map((post) => (
         <Post
-          key={post.userName}
+          key={post.id}
           avatarUrl={post.avatarUrl}
           userName={post.userName}
           userRole={post.userRole}
