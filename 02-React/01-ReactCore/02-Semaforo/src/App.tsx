48875f554e59df0e81273d966f2c0c6a67f6c427
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 03:40:41 -0400

    :fire:

---
+++ 02-React/01-ReactCore/02-Semaforo/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { TrafficLight } from './Components/TrafficLight'
+
+function App() {
+  return (
+    <div className="App">
+      <TrafficLight />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-React/01-ReactCore/02-Semaforo/src/Components/TrafficLight/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export function TrafficLight() {
+  return (
+    <div className="main">
+      <div className="content">
+        <h1>aa</h1>
+        <div className="red"></div>
+        <div className="yellow"></div>
+        <div className="green"></div>
+      </div>
+    </div>
+  )
+}
