3b581c0dd3638cdb108366ad66d992ac6230c4fd
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 02:20:52 -0400

    :fire: early return

--- 02-React/01-ReactCore/01-SocialPosts/src/components/Like/index.tsx
+++ 02-React/01-ReactCore/01-SocialPosts/src/components/Like/index.tsx
@@ -5,23 +5,27 @@ type LikeProps = {
 }
 
 export function Like({ liked, like, unLike }: LikeProps) {
-  return liked ? (
-    <svg
-      onClick={unLike}
-      xmlns="http://www.w3.org/2000/svg"
-      width="44"
-      height="44"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      fill="#ff5e95"
-      stroke="#ff5e95"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-    </svg>
-  ) : (
+  if (liked) {
+    return (
+      <svg
+        onClick={unLike}
+        xmlns="http://www.w3.org/2000/svg"
+        width="44"
+        height="44"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        fill="#ff5e95"
+        stroke="#ff5e95"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+      </svg>
+    )
+  }
+
+  return (
     <svg
       onClick={like}
       xmlns="http://www.w3.org/2000/svg"
