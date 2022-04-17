2c848c3fada09b6a7be426e9369e45abb5976bcb
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 04:32:25 -0400

    :bento:

--- 02-React/01-ReactCore/02-Semaforo/src/Components/TrafficLight/index.tsx
+++ 02-React/01-ReactCore/02-Semaforo/src/Components/TrafficLight/index.tsx
@@ -4,25 +4,39 @@ import './styles.css'
 
 export function TrafficLight() {
   const [message, setMessage] = useState('')
+  const [keyColor, setKeyColor] = useState<'red' | 'yellow' | 'green'>()
+
   function handleRedLightClick() {
     setMessage('Cuidado! Espere o sinal abrir')
+    setKeyColor('red')
   }
 
   function handleYellowLightClick() {
     setMessage('Atenção! Está prestes a ficar vermelho')
+    setKeyColor('yellow')
   }
 
   function handleGreenLightClick() {
     setMessage('Pode passar!')
+    setKeyColor('green')
   }
 
   return (
     <main>
       <div className="container">
         <div className="content">
-          <button className="red" onClick={handleRedLightClick}></button>
-          <button className="yellow" onClick={handleYellowLightClick}></button>
-          <button className="green" onClick={handleGreenLightClick}></button>
+          <button
+            className={`red ${keyColor === 'red' && 'selected'}`}
+            onClick={handleRedLightClick}
+          ></button>
+          <button
+            className={`yellow ${keyColor === 'yellow' && 'selected'}`}
+            onClick={handleYellowLightClick}
+          ></button>
+          <button
+            className={`green ${keyColor === 'green' && 'selected'}`}
+            onClick={handleGreenLightClick}
+          ></button>
         </div>
       </div>
 
