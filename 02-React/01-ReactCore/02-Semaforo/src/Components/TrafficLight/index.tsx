1188401ff38fa815417f020c0f5dddd34fe50e24
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-17 03:55:18 -0400

    :fire:

--- 02-React/01-ReactCore/02-Semaforo/src/Components/TrafficLight/index.tsx
+++ 02-React/01-ReactCore/02-Semaforo/src/Components/TrafficLight/index.tsx
@@ -4,7 +4,6 @@ export function TrafficLight() {
   return (
     <div className="main">
       <div className="content">
-        <h1>aa</h1>
         <div className="red"></div>
         <div className="yellow"></div>
         <div className="green"></div>
