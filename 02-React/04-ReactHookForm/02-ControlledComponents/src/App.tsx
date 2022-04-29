cc587d91ec98551650738f29b30b4b75c4415136
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-04-29 19:04:19 -0400

    :fire: controlled components

---
+++ 02-React/04-ReactHookForm/02-ControlledComponents/src/App.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    console.log(name)
+    console.log(email)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Type your name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Type your email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </header>
+    </div>
+  )
+}
+
+export default App
