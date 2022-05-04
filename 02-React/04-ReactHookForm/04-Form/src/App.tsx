c3abe8b33b1f4c1ab2992be25d0e102fb91660f5
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-05-04 00:46:48 -0400

    :fire: connect form in handler

--- 02-React/04-ReactHookForm/04-Form/src/App.tsx
+++ 02-React/04-ReactHookForm/04-Form/src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
 import './App.css'
-
 import { Title, FormGroup, SubmitButton } from './components/styles'
-
 import ReactInputMask from 'react-input-mask'
+import { Controller, useForm } from 'react-hook-form'
+
+interface IForm {
+  name: string
+  email: string
+  cpf: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { register, handleSubmit, control, formState } = useForm<IForm>({
+    mode: 'all',
+    delayError: 200,
+  })
+
+  function onSubmitHandler(values: any) {
+    console.log(values)
+  }
 
   return (
     <div className="App">
       <Title>Complete seus dados</Title>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
         <FormGroup>
           <label htmlFor="name">Nome</label>
-          <input type="text" name="name" placeholder="Insira o nome" />
+          <input
+            type="text"
+            placeholder="Insira o nome"
+            {...register('name', { required: true })}
+          />
         </FormGroup>
 
         <FormGroup>
           <label htmlFor="email">E-mail</label>
-          <input type="email" name="email" placeholder="Insira o e-mail" />
+          <input
+            type="email"
+            placeholder="Insira o e-mail"
+            {...register('email', { required: true })}
+          />
         </FormGroup>
 
         <FormGroup>
           <label htmlFor="cpf">CPF</label>
-          <ReactInputMask
+          <Controller
             name="cpf"
-            mask="999.999.999-999"
-            placeholder="Insira o CPF"
+            control={control}
+            defaultValue=""
+            render={({ field }) => {
+              return (
+                <ReactInputMask
+                  mask="999.999.999-999"
+                  placeholder="Insira o CPF"
+                  {...register('cpf')}
+                  {...field}
+                />
+              )
+            }}
           />
         </FormGroup>
 
